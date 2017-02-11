537f8811e72803b782649981db6267ad96bb6256
AionCode <astojanovic84@gmail.com>
2017-02-12 00:43:19 +0100

    Added more stuff

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+			</svg>
+		</div> <!-- .cd-svg-cover -->
+	</section> <!-- .cd-slider-wrapper -->
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/snap.svg-min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
